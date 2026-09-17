8f608caeb43ebe4321e75ceea3ae96055d5cdcd0
Juan Camilo Mona Lujan <camilo.mona.lujan@gmail.com>
2026-09-16 22:58:15 -0500

    feat: creacion inicial del proyecto maven

---
+++ dev-social-network/src/main/java/com/codefactory/dev_social_network/DevSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.codefactory.dev_social_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSocialNetworkApplication.class, args);
+	}
+
+}
---
+++ dev-social-network/src/test/java/com/codefactory/dev_social_network/DevSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefactory.dev_social_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
