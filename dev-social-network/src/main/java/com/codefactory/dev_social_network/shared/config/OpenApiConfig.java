ced70174d39bc52b9913472c6aab5d4034ecab2c
diegoDaSa97 <dalexander.sanchez@udea.edu.co>
2026-09-20 14:11:13 -0500

    fix: correct OpenAPI configuration package

--- dev-social-network/src/main/java/com/codefactory/dev_social_network/shared/config/OpenApiConfig.java
+++ dev-social-network/src/main/java/com/codefactory/dev_social_network/shared/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.codefactory.backend.config;
+package com.codefactory.dev_social_network.shared.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
